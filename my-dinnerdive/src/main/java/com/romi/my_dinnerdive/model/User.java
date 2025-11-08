e60e3a2e24d1146b81dc757b0db2d90b3c5672df
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-11-08 14:30:13 +0800

    [!]重構model

--- my-dinnerdive/src/main/java/com/romi/my_dinnerdive/model/User.java
+++ my-dinnerdive/src/main/java/com/romi/my_dinnerdive/model/User.java
@@ -18,12 +18,11 @@ public class User {
     private Integer userId;
 
     /** 使用者帳號 */
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
     /** 使用者密碼 */
     @JsonIgnore
-    @Column(nullable = false)
     private String userPassword;
 
     /** 使用者角色 */
@@ -41,36 +40,47 @@ public class User {
     public Integer getUserId() {
         return userId;
     }
+
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getUserPassword() {
         return userPassword;
     }
+
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
+
     public UserCategory getRoles() {
         return roles;
     }
+
     public void setRoles(UserCategory roles) {
         this.roles = roles;
     }
+
     public Date getCreatedDate() {
         return createdDate;
     }
+
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
     }
+
     public Date getLastModifiedDate() {
         return lastModifiedDate;
     }
+
     public void setLastModifiedDate(Date lastModifiedDate) {
         this.lastModifiedDate = lastModifiedDate;
     }
